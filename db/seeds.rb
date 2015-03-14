387f182ff3b40c9bf605a9f3635a19255f567d41
Shimon Mazor <shiramy@gmail.com>
2015-03-14 15:24:17 -0400

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 
 User.destroy_all
 User.create ([{ name: 'Ari Kalfus', username: 'dev1', password: 'devpass', email: 'dev1@dev.com', phone: '8005555555' }])
+User.create ([{ name: 'Shimon Mazor', username: 'shiramy', password: '1234567890', email: 'shiramy@gmail.com', phone: '8005555777' }])
 
 user = User.where(username: 'dev1').take
 
