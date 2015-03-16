6f2f4f7d3db129983ee431017aaad5150b7fe6d3
Aviv <aviv@totango.com>
2015-03-15 21:22:17 -0400

    last

--- app.rb
+++ app.rb
@@ -87,8 +87,13 @@ end
 
 get '/nanotwitter/v1.0/users/:username/profile' do
   if session[:user] # If user has credentials saved in session cookie (is logged in)
-    followees = Follow.where follower_id: session[:user][:id]
-    erb :user_profile, :locals => { :user => session[:user], :followees => followees }
+    user = UserService.get_by_username params[:username]
+    if session[:user][:username] == user[:username] #if user is watching their own page
+      followees = Follow.where follower_id: session[:user][:id]
+      erb :user_profile, :locals => { :user => session[:user], :followees => followees }
+    else
+      redirect to '/'
+    end
   else
     redirect to '/'
   end
