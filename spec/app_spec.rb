0f9484830493894d189d47c21f0461089e932c8f
Aviv <aviv@totango.com>
2015-03-21 14:19:31 -0400

    tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -35,9 +35,7 @@ describe 'app' do
       assert @browser.last_response.ok?
 
       assert @browser.last_request.env['rack.session'][:user].must_equal @user[:id], 'Session user id is not equal to user'
-
     end
-
   end
 
   describe 'GET on /' do
@@ -61,16 +59,13 @@ describe 'app' do
       assert @browser.last_response.ok?
       assert @browser.last_request.env["rack.session"][:user].must_equal @logged_in_user[:id]
     end
-
-
   end
 
 
 
 
-    describe "POST on /nanotwitter/v1.0/users/:username/follow" do 
+  describe "POST on /nanotwitter/v1.0/users/:username/follow" do 
 
-<<<<<<< HEAD
     before do
     # create a user and 2 tweets
       @logged_in_user = User.create({ name: 'Jerry Test',
@@ -85,9 +80,6 @@ describe 'app' do
       @browser.post '/nanotwitter/v1.0/users/session', { :username => 'jertest4', :password => 'jerrypass' }
       @browser.follow_redirect!
     end
-=======
-      end
->>>>>>> master
 
     it 'verify cookie is present' do
       @browser.get '/'
@@ -109,8 +101,6 @@ describe 'app' do
       @browser.follow_redirect!
       assert @browser.last_response.ok?
       assert @logged_in_user.following?(@followee)
-
-
     end
   
   end
@@ -134,7 +124,6 @@ describe 'app' do
       @browser.follow_redirect!
     end
 
-<<<<<<< HEAD
     it 'verify cookie is present' do
       @browser.get '/'
       assert @browser.last_response.ok?
@@ -157,65 +146,52 @@ describe 'app' do
 
       assert @logged_in_user.following?(@followee).must_equal false
     end
-  
   end
 
 
 
 
 
-end
 
-=======
-    describe 'GET on /logout and GET on /nanotwitter/v1.0/logout' do
+  describe 'GET on /logout and GET on /nanotwitter/v1.0/logout' do
 
-      before do
-        @logged_in_user = User.create({ name: 'Jerry Test',
-                                        username: 'jertest4',
-                                        password: 'jerrypass',
-                                        phone: 1234567890,
-                                      })
-
-        Tweet.create([{user_id: @logged_in_user[:id], text: 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!' },
-                      {user_id: @logged_in_user[:id], text: 'Hello world!'}
-                     ])
-        # save user session cookie
-        @browser.post '/nanotwitter/v1.0/users/session', { :username => 'jertest4', :password => 'jerrypass' }
-        @browser.follow_redirect!
-      end
-
-      it 'loads the logout page and delete the user from session' do
-        @browser.get '/logout'
-        @browser.follow_redirect!
-        @browser.follow_redirect!
-        assert @browser.last_response.ok?, 'response is not 200'
-
-        #Asserts that the user was deleted from the current session cookie
-        assert @browser.last_request.env["rack.session"][:user].must_be_nil, 'did not delete the user from session'
-
-        #Asserts that the rendered page has the logged out message and that it loads the recent tweets from the database
-        assert @browser.last_response.body.must_include 'You have been logged out.', 'did not include: "you have been logged out."'
-        assert @browser.last_response.body.must_include 'Hello world', 'did not include: "hello world"'
-      end
-
-      it 'loads the API logout page and delete the user from session' do
-        @browser.get '/nanotwitter/v1.0/logout'
-
-        #Asserts that the user was deleted from the current session cookie
-        assert @browser.last_request.env["rack.session"][:user].must_be_nil, 'did not delete the user from session'
-
-        #asserts that browser redirect to /logout
-        @browser.follow_redirect!
-        assert @browser.last_response.ok?
-
-        #Asserts that the browser redirected back to /logout
-        assert @browser.last_request.env["PATH_INFO"].must_equal "/logout", 'did not request /logout'
-
-        #Asserts that the rendered page has the logged out message and that it loads the recent tweets from the database
-        assert @browser.last_response.body.must_include 'You have been logged out.', 'did not include: "you have been logged out."'
-        assert @browser.last_response.body.must_include 'Hello world', 'did not include: "hello world"'
-      end
+    before do
+      @logged_in_user = User.create({ name: 'Jerry Test',
+                                      username: 'jertest4',
+                                      password: 'jerrypass',
+                                      phone: 1234567890,
+                                    })
+      Tweet.create([{user_id: @logged_in_user[:id], text: 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!' },
+                    {user_id: @logged_in_user[:id], text: 'Hello world!'}
+                   ])
+      # save user session cookie
+      @browser.post '/nanotwitter/v1.0/users/session', { :username => 'jertest4', :password => 'jerrypass' }
+      @browser.follow_redirect!
+    end
+    it 'loads the logout page and delete the user from session' do
+      @browser.get '/logout'
+      @browser.follow_redirect!
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?, 'response is not 200'
+      #Asserts that the user was deleted from the current session cookie
+      assert @browser.last_request.env["rack.session"][:user].must_be_nil, 'did not delete the user from session'
+      #Asserts that the rendered page has the logged out message and that it loads the recent tweets from the database
+      assert @browser.last_response.body.must_include 'You have been logged out.', 'did not include: "you have been logged out."'
+      assert @browser.last_response.body.must_include 'Hello world', 'did not include: "hello world"'
+    end
+    it 'loads the API logout page and delete the user from session' do
+      @browser.get '/nanotwitter/v1.0/logout'
+      #Asserts that the user was deleted from the current session cookie
+      assert @browser.last_request.env["rack.session"][:user].must_be_nil, 'did not delete the user from session'
+      #asserts that browser redirect to /logout
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+      #Asserts that the browser redirected back to /logout
+      assert @browser.last_request.env["PATH_INFO"].must_equal "/logout", 'did not request /logout'
+      #Asserts that the rendered page has the logged out message and that it loads the recent tweets from the database
+      assert @browser.last_response.body.must_include 'You have been logged out.', 'did not include: "you have been logged out."'
+      assert @browser.last_response.body.must_include 'Hello world', 'did not include: "hello world"'
     end
   end
+  
 end
->>>>>>> master
