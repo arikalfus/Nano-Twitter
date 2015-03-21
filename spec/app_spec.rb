e312be32dd9be6b524241957a2b5b81a20cd8e17
Aviv <aviv@totango.com>
2015-03-21 17:19:19 -0400

    final tests march 21

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -105,9 +105,6 @@ describe 'app' do
                                       password: 'jerrypass',
                                       phone: 1234567890
                                    })
-      Tweet.create([{user_id: @logged_in_user[:id], text: 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!' },
-                   {user_id: @logged_in_user[:id], text: 'Hello world!'}
-                   ])
       # save user session cookie
       @browser.post '/nanotwitter/v1.0/users/session', { :username => 'jertest4', :password => 'jerrypass' }
       @browser.follow_redirect!
@@ -146,9 +143,6 @@ describe 'app' do
                                       password: 'jerrypass',
                                       phone: 1234567890
                                    })
-      Tweet.create([{user_id: @logged_in_user[:id], text: 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!' },
-                   {user_id: @logged_in_user[:id], text: 'Hello world!'}
-                   ])
       # save user session cookie
       @browser.post '/nanotwitter/v1.0/users/session', { :username => 'jertest4', :password => 'jerrypass' }
       @browser.follow_redirect!
@@ -176,6 +170,7 @@ describe 'app' do
 
       assert @logged_in_user.following?(@followee).must_equal false
     end
+
   end
 
   describe 'GET on /logout and GET on /nanotwitter/v1.0/logout' do
@@ -253,5 +248,273 @@ describe 'app' do
       assert html_text.must_include('Follow')
     end
   end
+
+    describe "POST on /nanotwitter/v1.0/users/id/:id/tweet" do 
+
+      before do
+      # create a user and 2 tweets
+        @user = User.create({           
+                              name: 'Jerry Test',
+                              username: 'jertest4',
+                              password: 'jerrypass',
+                              phone: 1234567890
+                            })
+      end
   
+      it 'create a new tweet' do
+        assert Tweet.find_by({:user_id => @user[:id], :text => 'Hello World!'}).must_equal nil
+        @browser.post "/nanotwitter/v1.0/users/id/#{@user[:id]}/tweet", {:tweet => 'Hello World!'}
+        @browser.follow_redirect!
+        @browser.last_response.ok?
+        assert Tweet.find_by({:user_id => @user[:id], :text => 'Hello World!'})
+  
+      end
+  
+    end
+  
+
+  describe "POST on /nanotwitter/v1.0/users" do
+
+    User.delete_all
+    it 'create a new user with valid credentials' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'Aviv@Devdev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error].must_be_nil, 'some credentials are invalid'      
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty name' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => '', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-n', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid name' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv ♣ Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-nalpha', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty email' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => '',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-e', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid email' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDevdev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-einvalid', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty username' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => '', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-u', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid username' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'Aviv♠Dev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-uascii', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty password' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-p', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid password' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d32♠1421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-pascii', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty password2' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-p2', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid password length' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '321',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-pl', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with non-matching passwords' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '3333211111143421',
+                                                      :password2 => '33d321421',
+                                                      :phone => 4453239920
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-pns', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with empty phone' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => ''
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-ph', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid phone characters' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => '44533534a3'
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-phint', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+    User.delete_all
+    it 'create a new user with invalid phone length' do
+      @browser.post "/nanotwitter/v1.0/users", {
+                                                      :name => 'Aviv Dev', 
+                                                      :email => 'AvivDev@dev.com',
+                                                      :username => 'AvivDev', 
+                                                      :password => '33d321421',
+                                                      :password2 => '33d321421',
+                                                      :phone => '445335343'
+                                               }
+      assert @browser.last_request.env['rack.session'][:reg_error][:error_codes].must_include 'r-phl', 'user name is empty'
+      assert @browser.last_response.location.must_equal 'http://example.org/', 'Redirect location is not root'
+      @browser.follow_redirect!
+      assert @browser.last_response.ok?
+    end
+
+  end
+
 end
