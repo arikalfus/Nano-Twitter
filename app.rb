d29d1148694d954c2392d338684f03a1d27622b9
Aviv <aviv@totango.com>
2015-03-29 14:03:22 -0400

    Implementing loader.io

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 enable :sessions
 set :session_secret, '48fa3729hf0219f4rfbf39hf2'
 
-get '/loaderio-7b84b69492913d259b5266ab9f52dea7/' do
-  erb 'loaderio-7b84b69492913d259b5266ab9f52dea7'
+# For loader.io 
+get '/loaderio-9499c2875579506814d76c6f83a8f7f8/' do
+  erb 'loaderio-9499c2875579506814d76c6f83a8f7f8'
 end
 
 get '/' do
