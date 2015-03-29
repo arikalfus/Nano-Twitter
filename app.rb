bec6d32855a93f337e40310fa5af60e272b2b302
Aviv <aviv@totango.com>
2015-03-29 12:25:12 -0400

    send_file

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ enable :sessions
 set :session_secret, '48fa3729hf0219f4rfbf39hf2'
 
 get '/loaderio-7b84b69492913d259b5266ab9f52dea7.txt' do
-  erb file.new("loaderio-7b84b69492913d259b5266ab9f52dea7.txt")
+  erb send_file '/loaderio-7b84b69492913d259b5266ab9f52dea7.txt'
 end
 
 get '/' do
