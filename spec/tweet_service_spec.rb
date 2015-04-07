3acc22547fb8fa8c78174d06e9538f3057609ffa
Ari Kalfus <arikalfus@gmail.com>
2015-04-07 19:05:49 -0400

    Attempting to squash bugs

--- spec/tweet_service_spec.rb
+++ spec/tweet_service_spec.rb
@@ -28,13 +28,12 @@ describe 'tweet_service' do
     tweet = TweetService.new({text: 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!',
                          user_id: @user[:id]})
 
-    full_tweets = []
     full_tweets = TweetService.tweets_by_user_id(@user[:id])
 
     puts "full_tweets:\n#{full_tweets.pretty_inspect}"
 
-    # assert full_tweets[0]["text"].must_equal 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!'
-    # assert full_tweets[0]["user_id"].must_equal @user[:id]
+    assert full_tweets[0]["text"].must_equal 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!'
+    assert full_tweets[0]["user_id"].must_equal @user[:id]
   end
 
 
