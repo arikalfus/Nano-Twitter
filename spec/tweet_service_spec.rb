a4d7d4d28c9b9fe5fd10700e926255f4f1e92e5f
Ari Kalfus <arikalfus@gmail.com>
2015-04-07 19:38:41 -0400

    testing

--- spec/tweet_service_spec.rb
+++ spec/tweet_service_spec.rb
@@ -30,10 +30,8 @@ describe 'tweet_service' do
 
     full_tweets = TweetService.tweets_by_user_id(@user[:id])
 
-    puts "full_tweets:\n#{full_tweets}"
-
-    assert full_tweets[0]["text"].must_equal 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!'
-    assert full_tweets[0]["user_id"].must_equal @user[:id]
+    assert full_tweets.first["text"].must_equal 'Try to parse the GB interface, maybe it will navigate the bluetooth sensor!'
+    assert full_tweets.first["user_id"].must_equal @user[:id]
   end
 
 
