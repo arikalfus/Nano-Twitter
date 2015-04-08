774f22a5ea5eb92f4b5dee5fdacedf1452857aa8
Ari Kalfus <arikalfus@gmail.com>
2015-04-08 09:22:11 -0400

    Removed print statement

--- app.rb
+++ app.rb
@@ -64,7 +64,6 @@ get '/' do
     erb :root, :locals => { :tweets => tweets, :reg_error => reg_error }
   else
     tweets = TweetService.tweets
-    puts "tweets: #{tweets.pretty_inspect}"
 
     erb :root, :locals => { :tweets => tweets }
   end
