e455661ffdf610ff8ba757db7d6e6f192b31396b
Ari Kalfus <arikalfus@gmail.com>
2015-04-15 20:00:27 -0400

    Fixed background not loading on heroku server

--- services/tweet_service.rb
+++ services/tweet_service.rb
@@ -9,66 +9,12 @@ class TweetService
     tweets = Tweet.where(user_id: user_id).limit(100).order created_at: :desc
 
     build_tweets tweets
-
-    # # Construct an array of [tweet, user] pairs.
-    # user_ids = []
-    # full_tweets = []
-    #
-    # tweets.each do |tweet|
-    #   user_ids.push tweet[:user_id]
-    # end
-    # # multi-get database call
-    # users = UserService.get_by_ids user_ids
-    #
-    # # To optimize full_tweet creation below
-    # user_hash = Hash.new
-    # users.each do |user|
-    #   user_hash[user[:id]] = user
-    # end
-    #
-    # tweets.each do |tweet|
-    #   tweet_user = user_hash[tweet[:user_id]] # should return nil if no key is found
-    #   if tweet_user.nil?
-    #     Tweet.destroy(tweet[:id])
-    #   else
-    #     full_tweets.push [tweet, tweet_user]
-    #   end
-    # end
-    #
-    # full_tweets
   end
 
   def self.tweets
     tweets = Tweet.limit(100).order created_at: :desc
 
     build_tweets tweets
-
-    # # Construct an array of [tweet, user] pairs.
-    # user_ids = []
-    # full_tweets = []
-    #
-    # tweets.each do |tweet|
-    #   user_ids.push tweet[:user_id]
-    # end
-    # # multi-get database call
-    # users = UserService.get_by_ids user_ids
-    #
-    # # To optimize full_tweet creation below
-    # user_hash = Hash.new
-    # users.each do |user|
-    #   user_hash[user[:id]] = user
-    # end
-    #
-    # tweets.each do |tweet|
-    #   tweet_user = user_hash[tweet[:user_id]] # should return nil if no key is found
-    #   if tweet_user.nil?
-    #     Tweet.destroy(tweet[:id])
-    #   else
-    #     full_tweets.push [tweet, tweet_user]
-    #   end
-    # end
-    #
-    # full_tweets
   end
 
   def self.new(params)
@@ -92,6 +38,7 @@ class TweetService
   # Constructs an array of [tweet, user] pairs.
   def self.build_tweets(tweets)
 
+    # Construct an array of [tweet, user] pairs.
     user_ids = []
     full_tweets = []
 
