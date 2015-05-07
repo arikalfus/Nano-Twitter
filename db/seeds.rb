d283ac4dfd4ab38ea209dd3838b6c069b0914bf3
Ari Kalfus <arikalfus@gmail.com>
2015-05-06 23:15:58 -0400

    Added test_user to seeds data. Fixed bug in user profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,6 +55,7 @@ User.create ([{ name: 'Ari Kalfus', username: 'dev1', password: 'devpass', email
 User.create ([{ name: 'Shimon Mazor', username: 'shiramy', password: '1234567890', email: 'shiramy@gmail.com', phone: '8005555777', image: 'https://scontent-iad.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/p720x720/10580071_10204378851046860_2286913440271103339_n.jpg?oh=5fe551881244484b0a6afd55213213d5&oe=557D6A4C' }])
 User.create ([{ name: 'Aviv Glick', username: 'gaviv', password: '123321', email: 'dev2@dev.com', phone: '3342242', image: 'https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/10958924_10152983397638820_4707932385997110852_n.jpg?oh=d486de94df2645d3860e1f1f6d7bcbd0&oe=55B34A90&__gda__=1434012661_d844a039210345cd9cfef75efa8e5321' }])
 User.create ([{ name: 'Toby Gray', username: 'tgray', password: '11223344', email: 'devtonby@dev.com', phone: '99322242', image: 'https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/15411_10204653461433290_4531476834591487136_n.jpg?oh=f48ad07f596f8709ae0263b2ede76f1b&oe=5576F6AA&__gda__=1433519828_c4dc52cbec6ceafdcffb08d86c409b3e' }])
+User.create({name:"test_user", username:"test_user", password:'test_user', email:'test_user@lakin.info', phone:'0000000', image:'http://robohash.org/jdsngjkng.png?size=300x300' })
 
 #Now we want to create 0-200 tweets per user (average 100) Tweet :id 1,2,3,4,...,200
 Tweet.destroy_all
